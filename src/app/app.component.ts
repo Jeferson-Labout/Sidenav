92b827fdf24bb06201f42fd4b4c2e8ae3cab5eab
Jeferson Labout <70985458+Jeferson-Labout@users.noreply.github.com>
2022-07-29 17:03:35 -0300

    Alteração Rota

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs';
 
 
 interface SideNavToggle {
@@ -11,6 +14,52 @@ interface SideNavToggle {
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  constructor(router: Router, titleService: Title) {
+    router.events
+
+      // ****************************************
+      // Filtramos apenas os eventos do tipo
+      // NavigationEnd que ocorre quando a
+      // navegação da rota é efetuada com sucesso.
+      // Para saber mais sobre sobre este eventos:
+      // https://angular.io/guide/router-reference#router-events
+      // ****************************************
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe((event: NavigationEnd | any) => {
+        console.log('URL ativa: ', event.url);
+
+        switch (event.url) {
+          case '/dashboard':
+            titleService.setTitle('DashBoard');
+            break;
+
+          case '/produtos':
+            titleService.setTitle('Produtos');
+            break;
+
+          case '/estatisticas':
+            titleService.setTitle('Estatísticas');
+            break;
+          case '/cupons':
+            titleService.setTitle('Cupons');
+            break;
+          case '/paginas':
+            titleService.setTitle('Páginas');
+            break;
+          case '/midia':
+            titleService.setTitle('Mídia');
+            break;
+          case '/configuracoes':
+            titleService.setTitle('Configurações');
+            break;
+
+          default:
+            titleService.setTitle('SideNav');
+            break;
+        }
+      });
+  }
   title = 'sidenav';
 
   isSideNavCollapsed = false;
