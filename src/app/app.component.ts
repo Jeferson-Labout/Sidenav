2c5756acada537204c66950cf78925e14865ca39
Jeferson-Labout <jefson1989@gmail.com>
2022-07-29 01:38:53 -0300

    projeto finalizado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 
+
+interface SideNavToggle {
+  screenWidth: number;
+  collapsed: boolean;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +12,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'sidenav';
+
+  isSideNavCollapsed = false;
+  screenWidth = 0;
+
+
+  onTogglesSideNav(data: SideNavToggle): void {
+    this.screenWidth = data.screenWidth;
+    this.isSideNavCollapsed = data.collapsed;
+
+  }
 }
---
+++ src/app/sidenav/nav-data.ts
@@ -0,0 +1,45 @@
+export const navbarData = [
+    {
+        routeLink: 'dashboard',
+        icon: 'fal fa-home',
+        label: 'Dashboard'
+
+    },
+    {
+        routeLink: 'produtos',
+        icon: 'fal fa-box-open',
+        label: 'Produtos'
+
+    },
+    {
+        routeLink: 'estatisticas',
+        icon: 'fal fa-chart-bar',
+        label: 'Estatisticas'
+
+    },
+    {
+        routeLink: 'cupons',
+        icon: 'fal fa-tags',
+        label: 'Cupons'
+
+    },
+    {
+        routeLink: 'paginas',
+        icon: 'fal fa-file',
+        label: 'Páginas'
+
+    },
+    {
+        routeLink: 'midia',
+        icon: 'fal fa-camera',
+        label: 'Mídia'
+
+    },
+    {
+        routeLink: 'configuracoes',
+        icon: 'fal fa-cog',
+        label: 'Configurações'
+
+    },
+
+];
